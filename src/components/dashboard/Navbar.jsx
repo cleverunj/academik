1cedc813e31b991e3fa9be3253a2254b268fd058
cleverto <47237973+cleverto@users.noreply.github.com>
2024-07-12 08:42:27 -0500

    dashboard2

--- src/components/dashboard/Navbar.jsx
+++ src/components/dashboard/Navbar.jsx
@@ -9,6 +9,7 @@ import {
   Col,
   Container,
   Form,
+  Image,
   Nav,
   NavDropdown,
   Navbar,
@@ -17,11 +18,25 @@ import {
   Toast,
 } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
+
 import logo from "../../assets/imagen/logo64-white.png";
+import modAcademico from "../../assets/imagen/mod-academico.png";
+import modGrados from "../../assets/imagen/mod-grados.png";
+import modEvaluacion from "../../assets/imagen/mod-evaluacion.png";
+import modEscalafon from "../../assets/imagen/mod-escalafon.png";
+import modBiblioteca from "../../assets/imagen/mod-biblioteca.png";
+import modCalidad from "../../assets/imagen/mod-calidad.png";
+import modCentroPre from "../../assets/imagen/mod-centro-pre.png";
+import modInformatica from "../../assets/imagen/mod-informatica.png";
+import modPago from "../../assets/imagen/mod-pago.png";
+import modDocumento from "../../assets/imagen/mod-documento.png";
+import modNoLectivas from "../../assets/imagen/mod-no-lectivas.png";
 
 const NavBar = (props) => {
   const [showGap, setShowGap] = useState(false);
   const handleCloseGap = () => setShowGap(false);
+  const [showNoti, setShowNoti] = useState(false);
+  const handleCloseNoti = () => setShowNoti(false);
 
   let navigate = useNavigate();
 
@@ -46,15 +61,15 @@ const NavBar = (props) => {
 
           <Navbar.Text className="justify-content-end">
             <div className="d-flex ">
-              <div className="me-4 align-content-center px-4">
-                <Nav.Link href="#action1">
+              <div className="me-2 align-content-center px-4">
+                <Nav.Link
+                  onClick={() => {
+                    setShowNoti(!showNoti);
+                  }}
+                  style={{ position: "relative" }}
+                >
                   <i class="bi bi-bell fs-3"></i>
-                  <Badge
-                    bg="warning text-dark"
-                    style={{ position: "absolute" }}
-                  >
-                    9+
-                  </Badge>
+                  <span className="notification-badge">9+</span>
                 </Nav.Link>
               </div>
               <div className="align-content-center">
@@ -105,8 +120,18 @@ const NavBar = (props) => {
           </Navbar.Text>
         </Container>
       </Navbar>
+
+      <Offcanvas show={showNoti} onHide={handleCloseNoti} placement={"end"}>
+        <Offcanvas.Header closeButton className="border-info border-2">
+          <Offcanvas.Title  >Notificaciones</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          
+        </Offcanvas.Body>
+      </Offcanvas>
+
       <Offcanvas
-        style={{ height: "380px", top: "95px" }}
+        style={{ height: "470px", top: "95px" }}
         className="border-bottom border-top rounded-4 shadow"
         backdrop={true}
         placement={"end"}
@@ -116,79 +141,196 @@ const NavBar = (props) => {
         <Offcanvas.Body>
           <Container>
             <Row>
-              <Col xs lg="4" style={{height:"85px"}} className="d-grid gap-2  p-1">
-                <Card className="btn btn-light border-0 text-cut-parent">
-                  <i class="bi bi-person-workspace  d-block mx-auto"></i>
+              <Col
+                xs
+                lg="4"
+                style={{ height: "105px" }}
+                className="d-grid gap-2  p-1"
+              >
+                <Card className="btn btn-light border-0  text-cut-parent">
+                  <Image
+                    className="mx-auto"
+                    style={{ width: "48px" }}
+                    src={modAcademico}
+                    rounded
+                  />
                   <Card.Text className="text-cut  ">Académico </Card.Text>
                 </Card>
               </Col>
-              <Col xs lg="4" style={{height:"85px"}} className="d-grid gap-2  p-1">
+              <Col
+                xs
+                lg="4"
+                style={{ height: "105px" }}
+                className="d-grid gap-2  p-1"
+              >
                 <Card className="btn btn-light border-0 text-cut-parent">
-                  <i class="bi bi-person-workspace  d-block mx-auto"></i>
+                  <Image
+                    className="mx-auto"
+                    style={{ width: "48px" }}
+                    src={modGrados}
+                    rounded
+                  />
                   <Card.Text className="text-cut">Grados y Titulos </Card.Text>
                 </Card>
               </Col>
-              <Col xs lg="4" style={{height:"85px"}} className="d-grid gap-2  p-1">
+              <Col
+                xs
+                lg="4"
+                style={{ height: "105px" }}
+                className="d-grid gap-2  p-1"
+              >
                 <Card className="btn btn-light border-0  text-cut-parent">
-                  <i class="bi bi-person-workspace  d-block mx-auto"></i>
+                  <Image
+                    className="mx-auto"
+                    style={{ width: "48px" }}
+                    src={modEvaluacion}
+                    rounded
+                  />
                   <Card.Text className="text-cut">Evaluación </Card.Text>
                 </Card>
               </Col>
             </Row>
             <Row>
-              <Col xs lg="4" style={{height:"85px"}} className="d-grid gap-2  p-1">
+              <Col
+                xs
+                lg="4"
+                style={{ height: "105px" }}
+                className="d-grid gap-2  p-1"
+              >
                 <Card className="btn btn-light border-0  text-cut-parent">
-                  <i class="bi bi-person-workspace  d-block mx-auto"></i>
+                  <Image
+                    className="mx-auto"
+                    style={{ width: "48px" }}
+                    src={modBiblioteca}
+                    rounded
+                  />
                   <Card.Text className="text-cut">Biblioteca </Card.Text>
                 </Card>
               </Col>
-              <Col xs lg="4" style={{height:"85px"}} className="d-grid gap-2  p-1">
+              <Col
+                xs
+                lg="4"
+                style={{ height: "105px" }}
+                className="d-grid gap-2  p-1"
+              >
                 <Card className="btn btn-light border-0  text-cut-parent">
-                  <i class="bi bi-person-workspace  d-block mx-auto"></i>
+                  <Image
+                    className="mx-auto"
+                    style={{ width: "48px" }}
+                    src={modEscalafon}
+                    rounded
+                  />
                   <Card.Text className="text-cut">Escalafon </Card.Text>
                 </Card>
               </Col>
-              <Col xs lg="4" style={{height:"85px"}} className="d-grid gap-2  p-1">
+              <Col
+                xs
+                lg="4"
+                style={{ height: "105px" }}
+                className="d-grid gap-2  p-1"
+              >
                 <Card className="btn btn-light border-0  text-cut-parent">
-                  <i class="bi bi-person-workspace  d-block mx-auto"></i>
+                  <Image
+                    className="mx-auto"
+                    style={{ width: "48px" }}
+                    src={modPago}
+                    rounded
+                  />
                   <Card.Text className="text-cut">Pagos Virtuales </Card.Text>
                 </Card>
               </Col>
             </Row>
             <Row>
-            <Col xs lg="4" style={{height:"85px"}} className="d-grid gap-2  p-1">
+              <Col
+                xs
+                lg="4"
+                style={{ height: "105px" }}
+                className="d-grid gap-2  p-1"
+              >
                 <Card className="btn btn-light border-0  text-cut-parent">
-                  <i class="bi bi-person-workspace  d-block mx-auto"></i>
+                  <Image
+                    className="mx-auto"
+                    style={{ width: "48px" }}
+                    src={modCalidad}
+                    rounded
+                  />
                   <Card.Text className="text-cut">Calidad</Card.Text>
                 </Card>
               </Col>
-              <Col xs lg="4" style={{height:"85px"}} className="d-grid gap-2  p-1">
+              <Col
+                xs
+                lg="4"
+                style={{ height: "105px" }}
+                className="d-grid gap-2  p-1"
+              >
                 <Card className="btn btn-light border-0  text-cut-parent">
-                  <i class="bi bi-person-workspace  d-block mx-auto"></i>
+                  <Image
+                    className="mx-auto"
+                    style={{ width: "48px" }}
+                    src={modCentroPre}
+                    rounded
+                  />
                   <Card.Text className="text-cut">Centro PRE </Card.Text>
                 </Card>
               </Col>
-              <Col xs lg="4" style={{height:"85px"}} className="d-grid gap-2  p-1">
+              <Col
+                xs
+                lg="4"
+                style={{ height: "105px" }}
+                className="d-grid gap-2  p-1"
+              >
                 <Card className="btn btn-light border-0  text-cut-parent">
-                  <i class="bi bi-person-workspace  d-block mx-auto"></i>
-                  <Card.Text className="text-cut">Informática e Idiomas </Card.Text>
+                  <Image
+                    className="mx-auto"
+                    style={{ width: "48px" }}
+                    src={modInformatica}
+                    rounded
+                  />
+                  <Card.Text className="text-cut">
+                    Informática e Idiomas{" "}
+                  </Card.Text>
                 </Card>
               </Col>
             </Row>
             <Row>
-              <Col xs lg="4" style={{height:"85px"}} className="d-grid gap-2 p-1 ">
+              <Col
+                xs
+                lg="4"
+                style={{ height: "105px" }}
+                className="d-grid gap-2 p-1 "
+              >
                 <Card className="btn btn-light border-0  text-cut-parent">
-                  <i class="bi bi-person-workspace  d-block mx-auto"></i>
+                  <Image
+                    className="mx-auto"
+                    style={{ width: "48px" }}
+                    src={modNoLectivas}
+                    rounded
+                  />
                   <Card.Text className="text-cut">No lectivas </Card.Text>
                 </Card>
               </Col>
-              <Col xs lg="4" style={{height:"85px"}} className="d-grid gap-2 p-1">
+              <Col
+                xs
+                lg="4"
+                style={{ height: "105px" }}
+                className="d-grid gap-2 p-1"
+              >
                 <Card className="btn btn-light border-0  text-cut-parent">
-                  <i class="bi bi-person-workspace  d-block mx-auto"></i>
+                  <Image
+                    className="mx-auto"
+                    style={{ width: "48px" }}
+                    src={modDocumento}
+                    rounded
+                  />
                   <Card.Text className="text-cut">Documentos</Card.Text>
                 </Card>
               </Col>
-              <Col xs lg="4" style={{height:"85px"}} className="d-grid gap-2  p-1"></Col>
+              <Col
+                xs
+                lg="4"
+                style={{ height: "105px" }}
+                className="d-grid gap-2  p-1"
+              ></Col>
             </Row>
           </Container>
         </Offcanvas.Body>
